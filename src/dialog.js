2831e811a697913cbfa3dbbf709d4b3a616e685a
Danila <dfllolipop@gmail.com>
2023-10-16 12:00:36 +0300

    task5

--- src/dialog.js
+++ src/dialog.js
@@ -1,4 +1,6 @@
-console.log('Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"');
-console.log('Walter White: "What is it?"');
-console.log('Jesse Pinkman: "It\'s a giant magnet!"');
-console.log('Walter White: "What the hell are we going to do with a giant magnet?"');
+const fdialog = ('Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"');
+const sdialog = ('Walter White: "What is it?"');
+const tdialog = ('Jesse Pinkman: "It\'s a giant magnet!"');
+const frdialog = ('Walter White: "What the hell are we going to do with a giant magnet?"');
+const dialog= fdialog + sdialog + tdialog + frdialog
+console.log(dialog)
